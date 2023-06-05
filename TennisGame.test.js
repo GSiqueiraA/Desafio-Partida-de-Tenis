2dd78bcd4b7326468c3e837990dc23964513d5b5
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2023-06-05 17:16:14 -0300

    Create TennisGame.test.js

---
+++ TennisGame.test.js
@@ -0,0 +1,74 @@
+const { TennisGame } = require('./TennisGame');
+
+test('Ambos os jogadores começam com zero pontos', () => {
+  const jogo = new TennisGame();
+  expect(jogo.getPontuacao()).toEqual({ jogador1: 0, jogador2: 0 });
+});
+
+test('Jogador marca 15 pontos corretamente', () => {
+  const jogo = new TennisGame();
+  jogo.marcarPonto(1);
+  expect(jogo.getPontuacao()).toEqual({ jogador1: 15, jogador2: 0 });
+});
+
+test('Jogador marca 30 pontos corretamente', () => {
+  const jogo = new TennisGame();
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  expect(jogo.getPontuacao()).toEqual({ jogador1: 30, jogador2: 0 });
+});
+
+test('Jogador marca 40 pontos corretamente', () => {
+  const jogo = new TennisGame();
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  expect(jogo.getPontuacao()).toEqual({ jogador1: 40, jogador2: 0 });
+});
+
+test('Jogador vence o game ao alcançar 40 pontos', () => {
+  const jogo = new TennisGame();
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1); // Jogador 1 alcança 40 pontos
+  expect(jogo.vencedorDoGame()).toEqual('Jogador 1');
+});
+
+test('Ocorre empate (deuce) quando ambos os jogadores alcançam 40 pontos', () => {
+  const jogo = new TennisGame();
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(2);
+  jogo.marcarPonto(2);
+  jogo.marcarPonto(2); // Jogador 2 alcança 40 pontos
+  expect(jogo.getPontuacao()).toEqual({ Jogador1: 40, Jogador2: 40 });
+  expect(jogo.vencedorDoGame()).toEqual('Empate');
+});
+
+test('Jogador em vantagem ganha o game corretamente', () => {
+  const jogo = new TennisGame();
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(2);
+  jogo.marcarPonto(2);
+  jogo.marcarPonto(2);
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  
+  expect(jogo.vencedorDoGame()).toBe('Jogador1');
+  
+  test('Jogador em vantagem ganha o game corretamente', () => {
+  const jogo = new TennisGame();
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(1);
+  jogo.marcarPonto(2);
+  jogo.marcarPonto(2);
+  jogo.marcarPonto(2);
+  jogo.marcarPonto(2);
+  jogo.marcarPonto(2);
+  
+  expect(jogo.vencedorDoGame()).toBe('Jogador2');
